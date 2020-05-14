4fcd9d9a45d496116d6d087343847703fd229ea3
Rishabh Karnad <rishabhk@geekyants.com>
2020-05-14 14:52:10 +0530

    Throw error to reproduce issue with stack traces

--- App.vue
+++ App.vue
@@ -1,6 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello',
+    };
+  },
+  methods: {
+    p() {
+      this.message += '!';
+    },
+  },
+  mounted() {
+    function t() {
+      throw new Error('Mock error');
+    }
+    t();
+    console.log(10 < 20);
+  },
+};
+</script>
+
 <template>
   <view class="container">
     <text class="text-color-primary">My Vue Native App</text>
+    <text class="text-color-primary">{{ message }}</text>
+    <button title="Press" @press="p" />
   </view>
 </template>
 
